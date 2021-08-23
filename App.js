c7db6b4efd0122e4295dddabbe0e0386f6f81135
Kubraylmzz <83674667+Kubraylmzz@users.noreply.github.com>
2021-08-23 16:19:03 +0300

    tasarim yapildi

--- App.js
+++ App.js
@@ -1,112 +1,74 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { Component } from 'react';
+import { View, Text,StyleSheet, Button,Image, FlatList} from 'react-native';
+import data from './data';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+export default class App extends Component {
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.images}>
+        <Image source={require('./src/assets/false.jpg')} 
+        style={{width:60,height:60,right:130 ,resizeMode:'contain'}}></Image>
+         <Image source={require('./src/assets/true.jpg')} 
+        style={{width:60,height:60,left:130 ,resizeMode:'contain'}}></Image>
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
+         <View style={styles.alan}>
+           <Text>{data[0].tr}</Text>
+         </View>
+         <View style={styles.buttons}>
+        <Button 
+        title= {data[2].en} 
+        color='#D9728C'
+        onPress={this.onPressDecrease}/>
+        <Button 
+        title= {data[1].en}
+        color='#D9728C' 
+        onPress={this.onPressDecrease}/>
+        <Button 
+        title= {data[0].en} 
+        color='#D9728C' 
+        onPress={this.onPressDecrease}/>
+        </View> 
+      </View>
+    );
+  }
+}
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container:{
+    backgroundColor: '#FCEEF9',
+    flex: 3,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  alan:{
+    width: '55%',
+    //  marginHorizontal: 100,
+    //  marginVertical: 200,
+    backgroundColor: 'white',
+    padding: 25,
+    borderRadius: 10,
+    shadowColor: 'black',
+    shadowOpacity: 2,
+    shadowRadius: 5,
+    shadowOffset:{
+      width: 0,
+      height: 10,
+    },
+    elevation: 10
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  buttons: {
+    flex:3,
+     width: '50%',
+    //  height: '100%',
+    justifyContent: 'space-evenly',
+    flexDirection: 'column'
   },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+  images:{
+    flex:1,
+    flexDirection:'row',
+    // justifyContent: 'space-evenly',
+  }
+})
---
+++ data.js
@@ -0,0 +1,178 @@
+export default [
+  {
+    _id: '5c52de4c0f4ed2ae1bff8739',
+    tr: 'gönüllü',
+    en: 'volunteer',
+  },
+  {
+    _id: '5c52de4cac35a17d8c0993c1',
+    tr: 'günlük',
+    en: 'daily',
+  },
+  {
+    _id: '5c52de4c54f2cb9b2eaca28e',
+    tr: 'harikulade',
+    en: 'marvellous',
+  },
+  {
+    _id: '5c52de4c27a7eaf7b64d44ca',
+    tr: 'bitki',
+    en: 'plant',
+  },
+  {
+    _id: '5c52de4c470775a1e4041379',
+    tr: 'bacak',
+    en: 'leg',
+  },
+  {
+    _id: '5c52de4c491139efcc4d75cf',
+    tr: 'emniyet kemeri',
+    en: 'seatbelt',
+  },
+  {
+    _id: '5c52de4cb83143009752997c',
+    tr: 'karar vermek',
+    en: 'decide',
+  },
+  {
+    _id: '5c52de4c4e4ae4ea190a4743',
+    tr: 'memur',
+    en: 'officer',
+  },
+  {
+    _id: '5c52de4c916c0ec176cc25ab',
+    tr: 'bıçak',
+    en: 'knife',
+  },
+  {
+    _id: '5c52de4cf9da515d14735190',
+    tr: 'ummak',
+    en: 'hope',
+  },
+  {
+    _id: '5c52de4c42ef39a57f1bd1ee',
+    tr: 'uçak',
+    en: 'plane',
+  },
+  {
+    _id: '5c52de4cfdf070b1c0f5ed34',
+    tr: 'suçlu',
+    en: 'guilty',
+  },
+  {
+    _id: '5c52de4c157415a10ad2f39e',
+    tr: 'ip',
+    en: 'rope',
+  },
+  {
+    _id: '5c52de4c94f0146ee1da0a39',
+    tr: 'kaybetmek',
+    en: 'lose',
+  },
+  {
+    _id: '5c52de4ccae10cfc264ea5e0',
+    tr: 'kaçış',
+    en: 'escape',
+  },
+  {
+    _id: '5c52de4c47d12f28075e233b',
+    tr: 'kazanmak',
+    en: 'win',
+  },
+  {
+    _id: '5c52de4c277356ce5898add2',
+    tr: 'fırça',
+    en: 'brush',
+  },
+  {
+    _id: '5c52de4c3da0e66a28848c49',
+    tr: 'sürdürmek',
+    en: 'maintain',
+  },
+  {
+    _id: '5c52de4cdc5398d1d0e2e613',
+    tr: 'kaynamak',
+    en: 'boil',
+  },
+  {
+    _id: '5c52de4c281e927761f597d8',
+    tr: 'reddetmek',
+    en: 'refuse',
+  },
+  {
+    _id: '5c52de4ca2eba72ff8b19bbf',
+    tr: 'fren',
+    en: 'brake',
+  },
+  {
+    _id: '5c52de4c8abcc5cfdf8a62f7',
+    tr: 'giyinmek',
+    en: 'wear',
+  },
+  {
+    _id: '5c52de4cd45a1e52209e3d35',
+    tr: 'sabun',
+    en: 'soap',
+  },
+  {
+    _id: '5c52de4ca30e999fd297f483',
+    tr: 'güvenmek',
+    en: 'rely',
+  },
+  {
+    _id: '5c52de4c135e3122a92916e4',
+    tr: 'güzel',
+    en: 'beautiful',
+  },
+  {
+    _id: '5c52de4c09d3e98855c67981',
+    tr: 'cinayet işlemek',
+    en: 'murder',
+  },
+  {
+    _id: '5c52de4c5717df3361de5d9e',
+    tr: 'iddia etmek',
+    en: 'claim',
+  },
+
+  {
+    _id: '5c52de4cb60cf682a8db9f87',
+    tr: 'çadır',
+    en: 'tent',
+  },
+  {
+    _id: '5c52de4cf7066656d5d162a6',
+    tr: 'sıra',
+    en: 'queue',
+  },
+  {
+    _id: '5c52de4ca51bb600c6579963',
+    tr: 'tanıtmak',
+    en: 'introduce',
+  },
+  {
+    _id: '5c52de4ca0ae72e0a293a294',
+    tr: 'taze',
+    en: 'fresh',
+  },
+  {
+    _id: '5c52de4c7f102459b57142b3',
+    tr: 'cesur',
+    en: 'brave',
+  },
+  {
+    _id: '5c52de4c970f2a6247995343',
+    tr: 'nokta',
+    en: 'point',
+  },
+  {
+    _id: '5c52de4c25048abc1d13e86d',
+    tr: 'sır',
+    en: 'secret',
+  },
+  {
+    _id: '5c52de4c4211b38a38eaac2b',
+    tr: 'varlıklı',
+    en: 'wealthy',
+  },
+];
